c0a408814f17ae48ca2f746c2a530ea6247c4b5b
Mahabub Islam <miprio101@gmail.com>
2019-08-28 12:59:24 +0600

    commit

--- app/services/sites_status_check_service.rb
+++ app/services/sites_status_check_service.rb
@@ -6,15 +6,28 @@ class SitesStatusCheckService
     def self.check
       Project.all.each do |project|
         project.sites.each do |site|
-          uri = URI.parse(site.link)  
-          site_res = open(uri)
-          puts site_res.status.first
-          site.update_attributes(status: site_res.status.first, last_checked: Time.now)       
-          unless site_res.status.first.eql?('400')
-            puts "Something is wrong with #{site.title}"
-            ErrorMailer.with(user: site.user, data: site).report.deliver
+          begin
+            uri = URI.parse(site.link)  
+            site_res = open(uri)
+            if site_res.status.first.eql?('200')
+              puts "here in update"  
+              site.update_attributes(status: site_res.status.first, last_checked: Time.now)
+            end
+          rescue => e
+            puts e
+            case e         
+            when Net::OpenTimeout
+              puts "Something is wrong with #{site.title}"
+              ErrorMailer.with(user: site.user, data: site).report.deliver
+            when OpenURI::HTTPError
+              puts "Something is wrong with #{site.title}"
+              ErrorMailer.with(user: site.user, data: site).report.deliver  
+            else
+              puts "Something is wrong with #{site.title}"
+              ErrorMailer.with(user: site.user, data: site).report.deliver 
+            end   
           end
-        end
       end 
     end
+  end
 end
